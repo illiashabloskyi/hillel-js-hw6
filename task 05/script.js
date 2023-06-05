b6cc19e2e8b7c6e3f1988cb17e2d79b566f81722
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-06-05 19:43:33 +0300

    update task05/script.js

---
+++ task 05/script.js
@@ -0,0 +1,14 @@
+// Дано двозначне число. Визначити, яка з його цифр більша: перша чи друга?
+a = parseInt(prompt("Введіть число:"));
+firstNumber = Math.floor(a / 10);
+lastNumber = a % 10;
+
+if (isNaN(a) || a === "" || a === null) {
+  alert("Щось пішло не так");
+} else if (firstNumber > lastNumber) {
+  alert(`Цифра ${firstNumber} більша за цифру ${lastNumber}`);
+} else if (lastNumber > firstNumber) {
+  alert(`Цифра ${lastNumber} більша за цифру ${firstNumber}`);
+} else {
+  alert(`Цифри рівні`);
+}
